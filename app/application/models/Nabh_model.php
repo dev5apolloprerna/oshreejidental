0fa3213ba22f64300cfd2c365511f5dc30f0e954
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-02-14 22:23:42 +0530

    try to show appointment type pdf in patient detail as nabh form and on click on that show pdf

--- app/application/models/Nabh_model.php
+++ app/application/models/Nabh_model.php
@@ -2,15 +2,26 @@
 
 class Nabh_model extends App_Model
 {
-    private $table = 'tblnabh_master';
+    private $master = 'tblnabh_master';
+    private $map    = 'tblappointment_type_pdf_master';
 
-    public function get_all()
+    // ✅ Only mapped pdf/html for appointment type
+    public function get_mapped_forms($appointment_type_id)
     {
-        return $this->db->order_by('pdf_id', 'DESC')->get($this->table)->result_array();
+        $appointment_type_id = (int)$appointment_type_id;
+
+        return $this->db
+            ->select('m.id, m.title, m.english_title, m.gujarati_title, m.english_file_name, m.gujarati_file_name')
+            ->from($this->master . ' m')
+            ->join($this->map . ' mp', 'mp.appointment_pdf_id = m.id', 'inner')
+            ->where('mp.appointment_type_id', $appointment_type_id)
+            ->order_by('mp.id', 'DESC')
+            ->get()
+            ->result_array();
     }
 
     public function get($id)
     {
-        return $this->db->where('pdf_id', (int)$id)->get($this->table)->row_array();
+        return $this->db->where('id', (int)$id)->get($this->master)->row_array();
     }
 }
