25008ad92c0e035432983713917daae1e36d606d
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-03-10 14:53:57 +0530

    all branch patient global search

--- app/application/logs/log-2026-03-10.php
+++ app/application/logs/log-2026-03-10.php
@@ -22,3 +22,56 @@ ERROR - 2026-03-10 02:38:21 --> Severity: error --> Exception: Call to a member 
 ERROR - 2026-03-10 02:38:59 --> Severity: error --> Exception: Call to a member function output() on null C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 87
 ERROR - 2026-03-10 02:49:27 --> Could not find the language line "Number"
 ERROR - 2026-03-10 02:56:19 --> Could not find the language line "Number"
+ERROR - 2026-03-10 12:40:33 --> Could not find the language line "Select Branch"
+ERROR - 2026-03-10 12:40:33 --> Could not find the language line "Super Admin"
+ERROR - 2026-03-10 12:40:33 --> Could not find the language line "Select"
+ERROR - 2026-03-10 12:40:34 --> Could not find the language line "Select Branch"
+ERROR - 2026-03-10 12:40:34 --> Could not find the language line "Super Admin"
+ERROR - 2026-03-10 12:40:34 --> Could not find the language line "Select"
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 27
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 28
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 29
+ERROR - 2026-03-10 12:43:36 --> Could not find the language line "Filter"
+ERROR - 2026-03-10 12:43:36 --> Could not find the language line "Reset"
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 68
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 85
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 102
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 119
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 136
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 154
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 172
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 190
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 208
+ERROR - 2026-03-10 12:43:36 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 226
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 27
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 28
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "date_range" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 29
+ERROR - 2026-03-10 13:01:19 --> Could not find the language line "Filter"
+ERROR - 2026-03-10 13:01:19 --> Could not find the language line "Reset"
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 68
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 85
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 102
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 119
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 136
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 154
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 172
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 190
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 208
+ERROR - 2026-03-10 13:01:19 --> Severity: Warning --> Undefined array key "from_date" C:\wamp64\www\oshreejidental\app\application\views\admin\dashboard\widgets\my_top_stats.php 226
+ERROR - 2026-03-10 07:31:21 --> Severity: error --> Exception: syntax error, unexpected token "@", expecting "]" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 115
+ERROR - 2026-03-10 07:32:08 --> Severity: error --> Exception: syntax error, unexpected token "if", expecting "]" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 116
+ERROR - 2026-03-10 08:12:01 --> Severity: error --> Exception: syntax error, unexpected token "if", expecting "]" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 116
+ERROR - 2026-03-10 13:53:06 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 13:53:06 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 13:53:11 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 13:53:11 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 13:53:23 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 13:53:23 --> Severity: error --> Exception: Call to undefined method Clients::fetch_global_patient_rows() C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 124
+ERROR - 2026-03-10 14:00:00 --> Severity: Warning --> Undefined variable $searchData C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 104
+ERROR - 2026-03-10 14:00:00 --> Severity: Warning --> Undefined variable $searchData C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 106
+ERROR - 2026-03-10 14:00:00 --> Severity: Warning --> Undefined variable $searchData C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 104
+ERROR - 2026-03-10 14:00:00 --> Severity: Warning --> Undefined variable $searchData C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 106
+ERROR - 2026-03-10 08:41:05 --> Severity: error --> Exception: syntax error, unexpected token "private" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 252
+ERROR - 2026-03-10 08:41:22 --> Severity: error --> Exception: syntax error, unexpected token "private" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 252
+ERROR - 2026-03-10 08:52:32 --> Severity: error --> Exception: syntax error, unexpected token "private" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 253
+ERROR - 2026-03-10 08:52:38 --> Severity: error --> Exception: syntax error, unexpected token "private" C:\wamp64\www\oshreejidental\app\application\controllers\admin\Clients.php 253
