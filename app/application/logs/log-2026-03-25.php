32494652349a098523409f16c0fdaeb6a72c6044
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-03-27 11:34:51 +0530

    add nabh all form and patient history

---
+++ app/application/logs/log-2026-03-25.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Select Branch"
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Select Branch"
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Super Admin"
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Super Admin"
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Select"
+ERROR - 2026-03-25 12:48:23 --> Could not find the language line "Select"
