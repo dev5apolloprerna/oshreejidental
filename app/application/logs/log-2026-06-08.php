22929be5c188112500ca06850d87e52ba95ae395
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-06-08 18:42:31 +0530

    changes in lead add form design issue and add branch id

--- app/application/logs/log-2026-06-08.php
+++ app/application/logs/log-2026-06-08.php
@@ -4,3 +4,6 @@ ERROR - 2026-06-08 16:29:40 --> Severity: Warning --> Undefined variable $login_
 ERROR - 2026-06-08 16:29:42 --> Severity: error --> Exception: Column 'zip' in field list is ambiguous C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 307
 ERROR - 2026-06-08 17:12:39 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
 ERROR - 2026-06-08 17:12:41 --> Severity: error --> Exception: Column 'zip' in field list is ambiguous C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 307
+ERROR - 2026-06-08 17:14:56 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
+ERROR - 2026-06-08 17:14:57 --> Severity: error --> Exception: Column 'zip' in field list is ambiguous C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 307
+ERROR - 2026-06-08 17:18:23 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
