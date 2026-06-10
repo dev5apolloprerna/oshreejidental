6b04fd13d8f379a95fba370532b14dfaccdd4fea
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-06-11 01:03:02 +0530

    changes in global patient serach from all branches

---
+++ app/application/logs/log-2026-06-11.php
@@ -0,0 +1,11 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-06-11 00:17:17 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
+ERROR - 2026-06-11 00:22:39 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
+ERROR - 2026-06-11 00:22:40 --> Severity: error --> Exception: Access denied for user 'u614622744_maninagar_db'@'localhost' (using password: YES) C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2026-06-11 00:22:40 --> Severity: error --> Exception: Access denied for user 'u614622744_maninagar_db'@'localhost' (using password: YES) C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2026-06-11 00:23:11 --> Severity: error --> Exception: Access denied for user 'u614622744_maninagar_db'@'localhost' (using password: YES) C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2026-06-11 00:23:18 --> Severity: error --> Exception: Access denied for user 'u614622744_maninagar_db'@'localhost' (using password: YES) C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 203
+ERROR - 2026-06-11 00:29:45 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
+ERROR - 2026-06-11 00:46:29 --> Severity: error --> Exception: You have an error in your SQL syntax; check the manual that corresponds to your MySQL server version for the right syntax to use near '`tblclients` c LEFT JOIN `tblcontacts` ct ON ct.userid = c.userid AND ct.is_prim' at line 1 C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 307
+ERROR - 2026-06-11 00:59:06 --> Severity: Warning --> Undefined variable $login_class C:\wamp64\www\oshreejidental\app\application\views\admin\includes\head.php 30
