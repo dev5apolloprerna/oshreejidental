652f6fc21eb8f3bb34fc4fecbab5dfccf62aa977
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-07-26 15:39:48 +0530

    database

---
+++ app/application/logs/log-2026-07-25.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-07-25 15:27:48 --> Severity: error --> Exception: Unknown column 'tblstaff.branch_id' in 'where clause' C:\wamp64\www\oshreejidental\app\system\database\drivers\mysqli\mysqli_driver.php 307
