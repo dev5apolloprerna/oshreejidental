742fddb6b72ed89b3243ba6ed8eabafdd94d95fd
dev5apolloprerna <dev4.apolloinfotech@gmail.com>
2026-08-27 13:40:01 +0530

    changes in invoice recent data come first changes and in appointmnent search it show dublicate patient solve that issue

---
+++ app/application/logs/log-2026-08-26.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-08-26 15:58:17 --> Could not find the language line "Select Branch"
+ERROR - 2026-08-26 15:58:17 --> Could not find the language line "Super Admin"
+ERROR - 2026-08-26 15:58:17 --> Could not find the language line "Select"
